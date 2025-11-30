6fd371641781afda8f612e14b1b3a7948ccb0e92
nildoneumann <leonildoneumann@gmail.com>
2025-11-30 07:38:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./manifest.json",
   "./service-worker.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -39,3 +39,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
